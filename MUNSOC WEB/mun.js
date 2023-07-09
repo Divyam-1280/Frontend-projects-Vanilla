0e9b8ccb97e78e081b6618709e1fc880df3feb11
Divyam-1280 <Divyamraj700@gmail.com>
2023-07-09 12:12:55 +0530

    MUNSOC Website

---
+++ MUNSOC WEB/mun.js
@@ -0,0 +1,16 @@
+let formbtn = document.querySelector('#login-btn');
+let loginform = document.querySelector('.login-form-container');
+let formclose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navlist');
+menu.addEventListener('click', () =>{
+    console.log(menu)
+    // menu.classList.toggle(`fa-solid fa-bars`);
+    navbar.classList.toggle('active');
+});
+formbtn.addEventListener('click', () =>{
+    loginform.classList.add('active');
+});
+formclose.addEventListener('click', () =>{
+    loginform.classList.remove('active');
+});
