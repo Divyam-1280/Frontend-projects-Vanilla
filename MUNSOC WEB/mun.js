ac3681f4f6fd7a733fac830f88cd01498eaa77c1
Divyam-1280 <Divyamraj700@gmail.com>
2023-07-15 02:00:40 +0530

    about section update

--- MUNSOC WEB/mun.js
+++ MUNSOC WEB/mun.js
@@ -17,16 +17,3 @@ formclose.addEventListener('click', () =>{
 
 // SWIPER
 
-var swiper = new Swiper(".slide-container", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
