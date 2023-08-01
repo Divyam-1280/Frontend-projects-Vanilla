2e8b4f0bdd8db41012fba9376d1efc8389554062
Divyam-1280 <Divyamraj700@gmail.com>
2023-08-02 00:11:32 +0530

    quotes done

--- QuoteGen/quote.js
+++ QuoteGen/quote.js
@@ -1,9 +1,29 @@
-const api_url = "https://api.quotable.io/quotes/random";
+let options = {
+    method: 'GET',
+    headers: { 'x-api-key': 'eJH4Q5Xb2w955MfUdaMy4A==8QhfUtsA4VgacIQv' }
+  }
+  
+  let url = 'https://api.api-ninjas.com/v1/quotes'
+  
+  
+  fetch(url,options)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          quote.innerHTML = data[0].quote;
+          author.innerHTML = data[0].author;
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
 
-async function getquote(url){
-    const response = await fetch(url);
-    var data = await response.json();
-    console.log(data);
+
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+function refreshPage() {
+    location.reload();
 }
 
-getquote(api_url);
+function tweet() {
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "----by" + author.innerHTML , "Tweet Window" , "width = 600 , height = 300");
+}
